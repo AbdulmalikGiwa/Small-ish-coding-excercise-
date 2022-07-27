313b127d3461cdd80e0b3fe33f142b08286c6491
Abdulmalik Giwa <giwa.omotola@gmail.com>
2022-07-27 07:47:41 +0100

    Restructure celery tasks

--- posts_service/server/tasks/tasks.py
+++ posts_service/server/tasks/tasks.py
@@ -22,20 +22,21 @@ class MLServerResponse(enum.Enum):
     name="check_foul_language",
     retry_backoff=True,
 )
-def check_foul_language(paragraphs: list, post_id: int):
+def check_foul_language(json_data, ml_service):
     # Request to dummy ML service predicting if any sentence contains offensive words.
     # Offsetting this to celery so it can be executed concurrently in background.
-    ml_service = Config.ML_API
-    for paragraph in paragraphs:
-        json_data = ast.literal_eval(json.dumps({"fragment": paragraph}))
-        try:
-            r = requests.post(url=ml_service, json=json_data)
-        except Exception:
-            return MLServerResponse.unavailable.value
-        if r.status_code == 200:
-            data = r.json()
-            if data.get("hasFoulLanguage"):
-                return MLServerResponse.true.value
+    try:
+        r = requests.post(url=ml_service, json=json_data)
+    except Exception:
+        return MLServerResponse.unavailable.value
+
+    if r.status_code == 200:
+        data = r.json()
+        if data.get("hasFoulLanguage"):
+            return MLServerResponse.true.value
         else:
-            return MLServerResponse.unavailable.value
-    return MLServerResponse.false.value
+            return MLServerResponse.false.value
+    else:
+        return MLServerResponse.unavailable.value
+
+
--- posts_service/server/utils.py
+++ posts_service/server/utils.py
@@ -1,7 +1,11 @@
+import ast
+import json
+
 from flask import jsonify
 from marshmallow import Schema, ValidationError, fields
 from tinydb.operations import set
 
+from .tasks.config import Config
 from .tasks.database import db
 from .tasks.tasks import MLServerResponse, check_foul_language
 
@@ -31,26 +35,29 @@ def create_post(result):
     result["hasFoulLanguage"] = None
     post_id = db.insert(result)
     paragraphs = result.get("paragraphs")
-    result = check_foul_language.delay(paragraphs, post_id).get()
-    if result == MLServerResponse.true.value:
-        db.update(
-            set("hasFoulLanguage", True),
-            doc_ids=[
-                post_id,
-            ],
-        )
-        response = build_response("success", db.get(doc_id=post_id))
-        return jsonify(response), 201
-    elif result == MLServerResponse.false.value:
-        db.update(
-            set("hasFoulLanguage", False),
-            doc_ids=[
-                post_id,
-            ],
-        )
-        response = build_response("success", db.get(doc_id=post_id))
-        return jsonify(response), 201
+    ml_service = Config.ML_API
+    for paragraph in paragraphs:
+        json_data = ast.literal_eval(json.dumps({"fragment": paragraph}))
+        result = check_foul_language.delay(json_data, ml_service).get()
+        if result == MLServerResponse.true.value:
+            db.update(
+                set("hasFoulLanguage", True),
+                doc_ids=[
+                    post_id,
+                ],
+            )
+            response = build_response("success", db.get(doc_id=post_id))
+            return jsonify(response), 201
+        elif result == MLServerResponse.false.value:
+            db.update(
+                set("hasFoulLanguage", False),
+                doc_ids=[
+                    post_id,
+                ],
+            )
+            response = build_response("success", db.get(doc_id=post_id))
+            return jsonify(response), 201
 
-    elif result == MLServerResponse.unavailable.value:
-        response = build_response("ML service Unavailable", {})
-        return jsonify(response), 502
+        elif result == MLServerResponse.unavailable.value:
+            response = build_response("ML service Unavailable", {})
+            return jsonify(response), 502
